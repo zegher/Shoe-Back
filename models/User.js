6b1ef11325a450beaa0664b36628d81bdbfe3bbb
zghr <r0878962@student.thomasmore.be>
2023-12-17 12:25:31 +0100

    Chore: added more details to user

--- models/User.js
+++ models/User.js
@@ -9,7 +9,25 @@ const Userschema = new Schema({
   password: {
     type: String,
     required: true,
-  }
+  },
+  //add email, shoe-size, adress, country
+  email: {
+    type: String,
+    required: true,
+  },
+  shoeSize: {
+    type: Number,
+    required: true,
+  },
+  adress: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  
 });
 //export the model to use it in index.js
 const Users = mongoose.model('Users', Userschema);
