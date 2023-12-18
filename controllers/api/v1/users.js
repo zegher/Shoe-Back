f664d690a81e7195f299f2e26820f23f06997023
zghr <r0878962@student.thomasmore.be>
2023-12-18 11:19:48 +0100

    Chore: buggfixes in login

--- controllers/api/v1/users.js
+++ controllers/api/v1/users.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
+require('dotenv').config();
 
 const login = async (req, res) => {
     try {
