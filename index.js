7c5043b4446caccbbf514c5a084aa708e895a394
zghr <r0878962@student.thomasmore.be>
2023-12-19 14:50:43 +0100

    Chore: add primus code

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+const http = require('http');
 const cors = require('cors');
 
 const app = express();
